67e3b53218caf7af6cd5fbb3b4475f28d11c74fa
Alexey Vorobyov <alexey.vorobyov@prezi.com>
2022-07-26 19:43:04 +0300

    imporvements
    – use row in the middle
    – useCallback for passing extra props like col/row
    – winner function definition
    not that perfect, but works for 3x3 board.
    – disabled state in the end
    – draw vs finished message

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -3,19 +3,19 @@ import styled from 'styled-components';
 
 interface CellProps {
     size: number;
-    // TODO: how to combine such case into 1 type? Or perhaps better have separate types for Cell/StyledCell, reusing what can be reused
-    index?: number;
-    "data-index"?: number;
     onCellClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
 }
 
-const StyledCell = styled.div<CellProps>`
+export const StyledCell = styled.div<CellProps>`
     width: ${props => props.size}px;
     height: ${props => props.size}px;
     box-shadow: 0 0 0 1px coral;
     margin: 0 0 1px 1px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 
-export default function Cell({ size, index, onCellClick }: CellProps) {
-    return <StyledCell data-index={index} size={size} onClick={onCellClick} />;
+export default function Cell({ size, onCellClick }: CellProps) {
+    return <StyledCell size={size} onClick={onCellClick} />;
 }
--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,12 +1,23 @@
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import styled from 'styled-components';
-import Cell from "./Cell";
+import Cell, { StyledCell } from "./Cell";
 
-const StyledGrid = styled.div`
-    display: grid;
-    justify-content: center;
-    grid-template-columns: auto auto auto;
+const MINIMUM_STEPS_TO_WIN = 5;
+const StyledGrid = styled.div<{ disabled: boolean; }>`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    ${props => props.disabled && `
+        ${StyledCell} {
+            background: grey;
+            pointer-events: none;
+        }
+    `}
+`;
+
+const Row = styled.div`
+    display: flex;
 `;
 
 interface GridProps {
@@ -14,47 +25,111 @@ interface GridProps {
 }
 
 type Turn = 'X' | 'O';
+type Board = Array<Array<string>>; // TODO: check whether it's a legit approach
 interface StateProps {
     turn: Turn;
     finished: boolean;
+    draw: boolean;
+    board: Board;
+    currentStep: number;
 }
+
 const initialState: StateProps = {
     turn: 'X',
     finished: false,
+    draw: false,
+    board: [],
+    currentStep: 1,
 };
 
-
-
 export default function Grid({ size }: GridProps) {
-    const [gameState, setGameState] = useState(initialState);
-    const { turn, finished } = gameState;
+    const [{ turn, finished, board, currentStep, maxSteps, draw }, setGameState] = useState(() => {
+        const board: Board = Array(size).fill(null).map(_ => Array(size).fill('-'));
+        const maxSteps = size * size;
+        return {
+            ...initialState,
+            board,
+            maxSteps,
+        };
+    });
 
-    const onCellClick = (e: React.MouseEvent<HTMLDivElement>) => {
-        console.log(e.currentTarget);
-        const { index } = e.currentTarget.dataset;
-        if (turn === 'X') {
-            e.currentTarget.textContent = turn;
-            setGameState(prevState => ({
-                ...prevState,
-                turn: 'O',
-            }));
-        } else {
-            e.currentTarget.textContent = turn;
-            setGameState(prevState => ({
-                ...prevState,
-                turn: 'X'
-            }));
+    const hasWinner = useCallback((row: number, col: number) => {
+        console.table(board);
+        // check horizontally
+        // row same, col to check vary
+        if (board[row][0] === turn && board[row][1] === turn && board[row][2] === turn) {
+            return true;
         }
-        console.log(`Cell with key ${index} clicked`);
-    };
+
+        // check vertically
+        // col same, row to check vary
+        if (board[0][col] === turn && board[1][col] === turn && board[2][col] === turn) {
+            return true;
+        }
+
+        // check 1 diagonal
+        if (board[0][0] === turn && board[1][1] === turn && board[2][2] === turn) {
+            return true;
+        }
+        // check 2 diagonal
+        if (board[2][0] === turn && board[1][1] === turn && board[2][0] === turn) {
+            return true;
+        }
+
+    }, [turn, board]);
+
+    const onCellClick = useCallback((cellNode: HTMLElement, row: number, col: number) => {
+        if (cellNode.textContent) return;
+
+        const nextTurn = turn === 'X' ? 'O' : 'X';
+        cellNode.textContent = turn;
+        board[row][col] = turn;
+
+        console.log(`Cell [${row}, ${col}]  clicked`, turn);
+
+        // CAN START CHECKING THE WINNER AT THIS POINT
+        if (currentStep >= MINIMUM_STEPS_TO_WIN) {
+            if (hasWinner(row, col)) {
+                console.log('FINISHED!');
+                setGameState(prevState => ({
+                    ...prevState,
+                    finished: true,
+                }));
+
+                return null;
+            } else if (currentStep === maxSteps) {
+                console.log('DRAW!');
+                setGameState(prevState => ({
+                    ...prevState,
+                    finished: true,
+                    draw: true,
+                }));
+
+                return null;
+            };
+        }
+
+        setGameState(prevState => ({
+            ...prevState,
+            turn: nextTurn,
+            board,
+            currentStep: currentStep + 1,
+        }));
+
+    }, [turn, board, currentStep, maxSteps, hasWinner]);
 
     return (
-        finished
-            ? <div>Game finished and the winner is {turn}</div>
-            : <StyledGrid>
-                {[...Array(size * size)].map((_, key) => (
-                    <Cell key={key} index={key} size={50} onCellClick={onCellClick} />
+        <>
+            {finished && <h4>Game finished {draw ? 'and it\'s draw' : `and the winner is ${turn}`}</h4>}
+            <StyledGrid disabled={finished}>
+                {[...Array(size)].map((_, row) => (
+                    <Row key={row}>
+                        {[...Array(size)].map((_, col) => (
+                            <Cell key={col} size={50} onCellClick={e => onCellClick(e.currentTarget, row, col)} />
+                        ))}
+                    </Row>
                 ))}
             </StyledGrid>
+        </>
     );
 }
