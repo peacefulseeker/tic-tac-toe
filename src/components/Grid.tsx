7fc5c177de9470e709d290087908ba8bceaf3da0
Alexey Vorobyov <alexey.vorobyov@prezi.com>
2022-07-26 10:40:00 +0300

    2. Basic initial state + filling with values

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,10 +1,7 @@
 
+import { useState } from "react";
 import styled from 'styled-components';
 import Cell from "./Cell";
-
-interface GridProps {
-    size: number;
-}
 
 const StyledGrid = styled.div`
     display: grid;
@@ -12,18 +9,52 @@ const StyledGrid = styled.div`
     grid-template-columns: auto auto auto;
 `;
 
+interface GridProps {
+    size: number;
+}
+
+type Turn = 'X' | 'O';
+interface StateProps {
+    turn: Turn;
+    finished: boolean;
+}
+const initialState: StateProps = {
+    turn: 'X',
+    finished: false,
+};
+
+
+
 export default function Grid({ size }: GridProps) {
+    const [gameState, setGameState] = useState(initialState);
+    const { turn, finished } = gameState;
+
     const onCellClick = (e: React.MouseEvent<HTMLDivElement>) => {
         console.log(e.currentTarget);
         const { index } = e.currentTarget.dataset;
+        if (turn === 'X') {
+            e.currentTarget.textContent = turn;
+            setGameState(prevState => ({
+                ...prevState,
+                turn: 'O',
+            }));
+        } else {
+            e.currentTarget.textContent = turn;
+            setGameState(prevState => ({
+                ...prevState,
+                turn: 'X'
+            }));
+        }
         console.log(`Cell with key ${index} clicked`);
     };
 
     return (
-        <StyledGrid>
-            {[...Array(size * size)].map((_, key) => (
-                <Cell key={key} index={key} size={50} onCellClick={onCellClick} />
-            ))}
-        </StyledGrid>
+        finished
+            ? <div>Game finished and the winner is {turn}</div>
+            : <StyledGrid>
+                {[...Array(size * size)].map((_, key) => (
+                    <Cell key={key} index={key} size={50} onCellClick={onCellClick} />
+                ))}
+            </StyledGrid>
     );
 }
